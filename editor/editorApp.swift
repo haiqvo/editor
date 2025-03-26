b5696cd9620c6370afb034db2e8a755844f4ad0b
haiqvo <haivo@outlook.com>
2025-03-25 20:43:04 -0700

    Initial Commit

---
+++ editor/editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  editorApp.swift
+//  editor
+//
+//  Created by Hai Vo on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ editorTests/editorTests.swift
@@ -0,0 +1,16 @@
+//
+//  editorTests.swift
+//  editorTests
+//
+//  Created by Hai Vo on 3/25/25.
+//
+
+import Testing
+
+struct editorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
